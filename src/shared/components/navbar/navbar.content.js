60c6d7e625b09c61596858009e1fa4a8b50a0dbc
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-03-08 11:00:37 +0700

    chore: update navbar brand

--- src/shared/components/navbar/navbar.content.js
+++ src/shared/components/navbar/navbar.content.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import logo from 'assets/images/pokeapi.png';
+
 const Content = () => {
   return (
     <nav
@@ -19,7 +21,7 @@ const NavbarBrand = () => {
   return (
     <div className="navbar-brand">
       <Link className="navbar-item" to="/">
-        PokeNuz
+        <img src={logo} alt="Poke API" />
       </Link>
     </div>
   );
